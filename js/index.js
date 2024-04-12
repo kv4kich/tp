62b2ec4589d6337780175c22b1602dd7ffd30e05
kv4kich <iliakozlov778@gmail.com>
2024-04-12 17:19:23 +0300

    Updates

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 import '../scss/style.css'
 
-import '../js/slider'
+import '../js/slider.js'
 import '../js/show-more.js'
 import '../../node_modules/swiper/swiper-bundle.js'
 import '../js/modals.js'
